585dad24d3d781bef9c45b0b0c7446b81cbdcdbb
LeMar, Jennifer <LeMar.Jennifer@principal.com>
2020-11-16 11:18:54 -0600

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import {fetchMovies, fetchBooks} from "./services";
+import {fetchWithTimeout, fetchMovies, fetchBooks} from "./services";
+
+const movies = require("./data/movies.json");
 
 const getBooksAndMovies = () => {
     return Promise.all([fetchBooks(), fetchMovies()])
         .then(([books, movies]) => ({
-            books, movies
+            books,
+            movies
         }))
         .catch(error => console.log("Error fetching books and movies", error));
 };
-
 const getBooksAndMoviesPromise = getBooksAndMovies();
 getBooksAndMoviesPromise.then(results => {
     console.log("getBooksAndMoviesPromise", results);
